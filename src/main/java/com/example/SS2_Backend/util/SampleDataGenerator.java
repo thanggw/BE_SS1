523676e239f00c76f7ec72da5720e4474774e7b7
suyttthideptrai <ntienthanhn@gmail.com>
2023-11-19 10:46:46 +0700

    Update SampleDataGenerator.java

--- src/main/java/com/example/SS2_Backend/util/SampleDataGenerator.java
+++ src/main/java/com/example/SS2_Backend/util/SampleDataGenerator.java
@@ -18,7 +18,9 @@ import java.util.Random;
  */
 
 public class SampleDataGenerator {
+
     public static void main(String[] args) {
+<<<<<<< HEAD
         // Generate Individuals data Randomly
         ArrayList<Individual> individuals = generateSampleIndividuals(12,4);
 
@@ -36,13 +38,33 @@ public class SampleDataGenerator {
 
         // Run algorithm:
         long startTime = System.currentTimeMillis();
+=======
+        ArrayList<Individual> individuals = generateSampleIndividuals(12);
+
+        // Create a StableMatchingProblem object with the generated data
+        StableMatchingProblem problem = new StableMatchingProblem(individuals, "compositeWeightFunction", "fitnessFunction");
+        System.out.println(problem);
+        System.out.println(problem.getPropertyValueOf(10, 1)); //success
+        System.out.println(problem.getPropertyWeightOf(10, 1)); // success
+        if(problem.isPreferenceEmpty()){
+            System.out.println("Preference failed to generate");
+        }else{
+            System.out.println("success");
+            System.out.println(problem.printPreferenceLists());
+        }
+        System.out.println(problem.getNumberOfIndividual()); //success
+        System.out.println(problem.printPreferenceLists()); //failed
+        System.out.println(problem.isPreferenceEmpty()); // true -- Preference initializing failed
+
+>>>>>>> Le-Thanh
         NondominatedPopulation result = new Executor()
                 .withProblem(problem)
                 .withAlgorithm("NSGAII")
-                .withMaxEvaluations(1000)
-                .withProperty("populationSize", 200)
+                .withMaxEvaluations(5)
+                .withProperty("populationSize", 5)
                 .distributeOnAllCores()
                 .run();
+<<<<<<< HEAD
         long endTime = System.currentTimeMillis();
         double runtime = ((double) (endTime - startTime) / 1000);
         runtime = Math.round(runtime * 100.0) / 100.0;
@@ -69,6 +91,18 @@ public class SampleDataGenerator {
             System.out.println("Fitness Score: " + -solution.getObjective(0));
         }
         System.out.println("\nExecution time: " + runtime + " Second(s) with Algorithm: " + "NSGAII");
+=======
+        for (Solution solution : result) {
+//            System.out.print(solution.getVariable(0).toString() + "\t\t|");
+            System.out.print(-solution.getObjective(0) + "\t"); // Negate to show maximized objective
+//            System.out.print(solution.getObjective(1));
+            System.out.println();
+        }
+//        Solution solution = problem.newSolution();
+//        System.out.println(solution.getVariable(0).toString());
+//        Matches matches = problem.stableMatching(solution);
+//        System.out.println(matches);
+>>>>>>> Le-Thanh
     }
 
     public static ArrayList<Individual> generateSampleIndividuals(int numIndividuals, int numProps) {
@@ -85,6 +119,7 @@ public class SampleDataGenerator {
             Individual individual = new Individual(individualName, individualSet);
 
             // Add some sample properties (you can customize this part)
+<<<<<<< HEAD
             for (int j = 0; j < numProps; j++) {
                 double propertyValue = new Random().nextDouble() * (70.0 - 20.0) + 20.0;
                 // Random property Value (20 -> 50)
@@ -102,6 +137,13 @@ public class SampleDataGenerator {
                     String[] requirement = {String.valueOf(propertyBound), String.valueOf(propertyBound2)};
                     individual.setProperty(propertyValue, propertyWeight, requirement);
                 }
+=======
+            for (int j = 0; j < 5; j++) { // Adding 5 sample properties for each individual
+                String propertyName = "Property" + j;
+                int propertyValue = new Random().nextInt(20) + 1;
+                int propertyWeight = new Random().nextInt(10) + 1; // Random weight between 1 and 10
+                individual.setProperty(propertyName, propertyValue, propertyWeight);
+>>>>>>> Le-Thanh
             }
 
             individuals.add(individual);
