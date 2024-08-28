cabdc19d1b139fddcbf5fdfb1e4d1ed99521c435
Thành <2101040168@ms.hanu.edu.vn>
2024-08-28 08:47:15 +0700

    Add Test

---
+++ src/test/java/com/example/SS2_Backend/util/SampleDataGeneratorTest.java
@@ -0,0 +1,11 @@
+package com.example.SS2_Backend.util;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleDataGeneratorTest {
+
+    @Test
+    void testWithSampleGeneratedData() {
+        SampleDataGenerator.main(new String[0]);
+    }
+}
